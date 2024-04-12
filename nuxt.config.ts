8db017c0c1a177904295638d54b294afb018140f
regtyit <titov54275@gmail.com>
2024-04-12 18:22:29 +0400

    lol commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+    }
+  },
   build: {
     transpile: ['vuetify'],
   },
