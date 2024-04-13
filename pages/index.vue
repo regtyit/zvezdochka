6c0d538a2dff2717f7161662c34769c71e1527e8
qbclub <17926057+qbclub@users.noreply.github.com>
2024-04-13 12:46:21 +0400

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,67 @@
 <script setup>
-  import { useDisplay } from 'vuetify'
+import { useDisplay } from 'vuetify'
 
-  const { md } = useDisplay()
+const { md } = useDisplay()
 
 </script>
 
 <template>
   <v-container>
-  
+
     <v-row class="start-page">
-      <v-col  class="ma-0 pa-0" cols="6" sm="4" md="3">
+      <v-col class="ma-0 pa-0" cols="6" sm="4" md="3">
         <div class="tile logo">
-          <v-img
-
-  
-  cover
-  src="../assets/images/logo.jpg"
-  style="border-radius: 10px;"
-></v-img>
+          <v-img cover src="../assets/images/logo.jpg" style="border-radius: 10px;"></v-img>
 
 
         </div>
 
       </v-col>
-      <v-col  class="ma-0 pa-0" cols="6" sm="4" md="3">
+      <v-col class="ma-0 pa-0" cols="6" sm="4" md="3">
         <div class="tile">
 
         </div>
 
       </v-col>
-      <v-col  class="ma-0 pa-0" cols="6" sm="4" md="3">
+      <v-col class="ma-0 pa-0" cols="6" sm="4" md="3">
         <div class="tile">
 
         </div>
 
       </v-col>
-      <v-col  class="ma-0 pa-0" cols="6" sm="4" md="3">
+      <v-col class="ma-0 pa-0" cols="6" sm="4" md="3">
         <div class="tile">
 
         </div>
 
       </v-col>
-      <v-col  class="ma-0 pa-0" cols="6" sm="4" md="3">
+      <v-col class="ma-0 pa-0" cols="6" sm="4" md="3">
         <div class="tile">
 
         </div>
 
       </v-col>
-      <v-col  class="ma-0 pa-0" cols="6" sm="4" md="3">
+      <v-col class="ma-0 pa-0" cols="6" sm="4" md="3">
         <div class="tile">
 
         </div>
 
       </v-col>
-      <v-col  class="ma-0 pa-0" cols="6" sm="4" md="3">
+      <v-col class="ma-0 pa-0" cols="6" sm="4" md="3">
         <div class="tile">
 
         </div>
 
       </v-col>
-      <v-col  class="ma-0 pa-0" cols="6" sm="4" md="3">
+      <v-col class="ma-0 pa-0" cols="6" sm="4" md="3">
         <div class="tile">
 
         </div>
 
       </v-col>
-      <v-col v-if="md" cols="6" sm="4" md="3" class="ma-0 pa-0" >
-        <div class="tile" >
-Доп тайл на среднем экране
+      <v-col v-if="md" cols="6" sm="4" md="3" class="ma-0 pa-0">
+        <div class="tile">
+          Доп тайл на среднем экране
 
         </div>
       </v-col>
@@ -84,15 +78,13 @@
 }
 
 .tile {
- border-radius: 5%;
- margin: 5px;
+  border-radius: 5%;
+  margin: 5px;
   aspect-ratio: 1;
   border: 1px solid black;
-  
-  
-}
-.logo{
- 
+
 
 }
+
+.logo {}
 </style>
