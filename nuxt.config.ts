feca7ff45b692af4f4c948c0e91e5a9523880060
qbclub <17926057+qbclub@users.noreply.github.com>
2024-04-13 10:22:07 +0400

    fix nuxt config err

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,14 +7,18 @@ export default defineNuxtConfig({
     }
   },
   build: {
-    transpile: ['vuetify'],
+    transpile: ["vuetify"],
   },
+
   modules: [
     (_options, nuxt) => {
-      nuxt.hooks.hook('vite:extendConfig', (config) => {
-        config.plugins.push(vuetify({ autoImport: true }))
-      })
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
     },
+   
+   
   ],
   vite: {
     vue: {
