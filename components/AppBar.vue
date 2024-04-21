034929229571de693be7bf72486312f26477ac22
qbclub <17926057+qbclub@users.noreply.github.com>
2024-04-21 09:29:34 +0400

    loadig logo

--- components/AppBar.vue
+++ components/AppBar.vue
@@ -1,12 +1,25 @@
 <template>
-    <v-app-bar elevation="0" color="#faebd7" >
+    <v-app-bar elevation="0" color="#faebd7">
         <v-container>
             <v-row>
                 <v-col>
                     <div class="d-flex align-center justify-space-between">
                         <div>
-                           <NuxtLink to="/"> <v-img cover src="../assets/images/long_logo.png" width="180" ></v-img></NuxtLink>
-                          
+                            <NuxtLink to="/">
+                                <v-img cover src="../assets/images/long_logo.png" width="180">
+                                    <template v-slot:placeholder>
+                                        <v-row class="fill-height ma-0" align="center" justify="center">
+                                            <v-progress-circular indeterminate></v-progress-circular>
+                                        </v-row>
+                                    </template>
+                                    <template v-slot:error>
+                                        <v-row class="fill-height ma-0" align="center" justify="center">
+                                            <span>Ошибка загрузки изображения</span>
+                                        </v-row>
+                                    </template>
+                                </v-img>
+                            </NuxtLink>
+
                         </div>
                         <v-app-bar-nav-icon></v-app-bar-nav-icon>
                     </div>
