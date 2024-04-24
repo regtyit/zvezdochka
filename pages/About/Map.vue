24ba42e0c54ca88048c5d2081751dff3138b9dfe
qbclub <17926057+qbclub@users.noreply.github.com>
2024-04-24 09:12:58 +0400

    add ya map

--- pages/About/Map.vue
+++ pages/About/Map.vue
@@ -2,15 +2,9 @@
     <v-container>
         <v-row>
             <v-col>
-                <div style="position:relative;overflow:hidden;"><a
-                        href="https://yandex.ru/maps/org/dzl_zvyozdochka/102488833298/?utm_medium=mapframe&utm_source=maps"
-                        style="color:#eee;font-size:12px;position:absolute;top:0px;">Дзл Звёздочка</a><a
-                        href="https://yandex.ru/maps/11148/udmurt-republic/category/summer_camp/184106418/?utm_medium=mapframe&utm_source=maps"
-                        style="color:#eee;font-size:12px;position:absolute;top:14px;">Детский лагерь отдыха в Удмуртской
-                        Республике</a><iframe
-                        src="https://yandex.ru/map-widget/v1/?ll=52.785018%2C58.133793&mode=search&oid=102488833298&ol=biz&z=12.95"
-                        width="100%" height="400" frameborder="0" allowfullscreen="true"
-                        style="position:relative;"></iframe></div>
+                <iframe
+                    src="https://yandex.ru/map-widget/v1/?um=constructor%3A26abf34e4557bdf0ecf24e2e21dda0ecbf242b4c17ebc4deb9509ab344020376&amp;source=constructor"
+                    width="100%" height="381" frameborder="0"></iframe>
 
             </v-col>
         </v-row>
